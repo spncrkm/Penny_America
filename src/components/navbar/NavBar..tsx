719de3494898ef48af91eb2e7d48b854b09ce004
Spencer Kim <spncrkm@gmail.com>
2024-08-26 21:33:21 -0400

    changed logo text size

--- src/components/navbar/NavBar..tsx
+++ src/components/navbar/NavBar..tsx
@@ -4,42 +4,42 @@ import Style from "./NavBar.module.css";
 import PlaidLink from "../PlaidLink";
 
 const NavBar = () => {
-  const username = localStorage.getItem('username') as string;
+  const username = localStorage.getItem("username") as string;
 
   return (
-      <div>
-        <div className={Style.sidebar}>
-          <div className={Style.brand}>
-            {/* <link href="/dashboard"><img src={piglogo} />  PennyAM</link> */}
-            <img src={piglogo} />  PennyAM
-          </div>
-          <div className={Style.member}>
-            <div className={Style.name_logout}>
+    <div>
+      <div className={Style.sidebar}>
+        <div className={Style.brand}>
+          {/* <link href="/dashboard"><img src={piglogo} />  PennyAM</link> */}
+          <img src={piglogo} /> PennyAM
+        </div>
+        <div className={Style.member}>
+          <div className={Style.name_logout}>
             <Logout />
             <h3>Hi, {username}</h3>
-            </div>
-            <p>Premium Member</p>
-            <PlaidLink />
-            <a href='#'>Dashboard</a>
-            <a href='#'>Budget Management</a>
-            <a href='#'>Savings Plan</a>
-            <a href='#'>Account</a>
-            <a href='#'>Transactions</a>
-            <hr/>
-            <a href="#">Setting</a>
-            <a href="#">Help Center</a>
-            <a href="#">Security and Privacy</a>
-            <div className={Style.navimg}>
-              <img src={navcellphone} />
-            </div>
           </div>
-
-          <div className={Style.chatSupport}>
-            <p>Welcome back</p>
-            <h3>{username}</h3>
+          <p>Premium Member</p>
+          <PlaidLink />
+          <a href="#">Dashboard</a>
+          <a href="#">Budget Management</a>
+          <a href="#">Savings Plan</a>
+          <a href="#">Account</a>
+          <a href="#">Transactions</a>
+          <hr />
+          <a href="#">Setting</a>
+          <a href="#">Help Center</a>
+          <a href="#">Security and Privacy</a>
+          <div className={Style.navimg}>
+            <img src={navcellphone} />
           </div>
         </div>
+
+        <div className={Style.chatSupport}>
+          <p>Welcome back</p>
+          <h3>{username}</h3>
+        </div>
       </div>
+    </div>
   );
 };
 
